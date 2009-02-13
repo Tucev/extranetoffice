30d0b3ae370cc052f1c621af4141393e433c5c66
luis.montero <luis.montero@e-noise.com>
2009-02-13 02:01:12 +0000

    Deleting unnecessary eclipse hidden files.

--- build-doc.sh
+++
@@ -1,5 +0,0 @@
-# Build documentation using phpdocumentor
-/Users/lupo/Sites/phpdoc/phpdoc -c ~/Sites/phpdoc/user/extranetoffice.ini
-
-# count lines of code
-/Users/lupo/Scripts/cloc-1.07.pl --exclude-dir=.cvs,.svn,lib,doc ~/Sites/internal/extranetoffice/
