398a8efac7eb0f5b9ad510521219aab004c9cc09
luis.montero <luis.montero@e-noise.com>
2009-02-15 19:13:00 +0000

    Pagination class in phpFrame updated (javadoc comments added) and getListFooter method split into 3 methods for the sake of clarity and flexibility.

--- lib/phpframe/html/pagination.php
+++ lib/phpframe/html/pagination.php
@@ -81,10 +81,43 @@ class pagination {
 	 * This method is invoked to print the pagination at the bottom of a data table.
 	 * 
 	 * @return	string	The HTML with the pagination footer
+	 * @access	public
+	 * @since	1.0
 	 */
-	function getListFooter() {
-		// Build limit select
-		$html = '<div>';
+	public function getListFooter() {
+		if ($this->pages > 1) {
+			// Build limit select
+			$html = '<div>';
+			$html .= $this->getLimitSelect();
+			$html .= '</div>';
+			
+			// Build page nav
+			$html .= '<div>';
+			$html .= $this->getPageNav();
+			$html .= '</div>';
+			
+			// Display current page number
+			$html .= '<div>';
+			$html .= $this->getPageInfo();
+			$html .= '</div>';
+			
+			return $html;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	/**
+	 * getLimitSelect()
+	 * 
+	 * This method returns a string with HTML containing a select input to select the available pages.
+	 * 
+	 * @return string
+	 * @access	public
+	 * @since	1.0
+	 */
+	public function getLimitSelect() {
 		$html .= '<form name="limitform" id="limitform" method="post">';
 		$html .= 'Display Num: ';
 		$html .= '<select name="limit" onchange="document.forms[\'limitform\'].submit();">';
@@ -98,16 +131,28 @@ class pagination {
 		$html .= '<option value="all">-- All --</option>';
 		$html .= '</select>';
 		$html .= '</form>';
-		$html .= '</div>';
 		
-		// Build page nav
+		return $html;
+	}
+	
+	/**
+	 * getPageNav()
+	 * 
+	 * This method returns a string with the HTML code containing the page navigation by page numbers.
+	 * 
+	 * Navigation elements are built into an unordered list of class "pageNav" (<ul class="pageNav">).
+	 * 
+	 * @return string
+	 * @access	public
+	 * @since	1.0
+	 */
+	public function getPageNav() {
 		$href = 'index.php?option='.request::getVar('option');
 		$href .= '&amp;view='.request::getVar('view');
 		$href .= '&amp;layout='.request::getVar('layout');
 		$href .= '&amp;limit='.$this->limit;
 		
-		$html .= '<div>';
-		$html .= '<ul>';
+		$html = '<ul>';
 		// Start link
 		$html .= '<li>';
 		if ($this->current_page != 1) {
@@ -155,12 +200,23 @@ class pagination {
 		}
 		$html .= '</li>';
 		$html .= '</ul>';
-		$html .= '</div>';
 		
-		// Display current page number
-		$html .= '<div>';
-		$html .= 'Page '.$this->current_page.' of '.$this->pages;
-		$html .= '</div>';
+		return $html;
+	}
+	
+	/**
+	 * getPageInfo()
+	 * 
+	 * This method returns a string with the current page number and total number of pages.
+	 * 
+	 * ie: Page 1 of 4
+	 * 
+	 * @return 	string
+	 * @access	public
+	 * @since	1.0
+	 */
+	public function getPageInfo() {
+		$html = 'Page '.$this->current_page.' of '.$this->pages;
 		
 		return $html;
 	}
