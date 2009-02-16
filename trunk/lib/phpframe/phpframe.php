a351ad3e7a4c38567b6350759938f8bc26adf619
luis.montero@e-noise.com <luis.montero@e-noise.com@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-02-16 02:15:08 +0000

    phpFrame overall working update.

--- trunk/lib/phpframe/phpframe.php
+++ trunk/lib/phpframe/phpframe.php
@@ -22,14 +22,15 @@ require_once _PHPFRAME_PATH.DS."environment".DS."request.php";
 require_once _PHPFRAME_PATH.DS."environment".DS."session.php";
 // Include application classes
 require_once _PHPFRAME_PATH.DS."application".DS."application.php";
-require_once _PHPFRAME_PATH.DS."application".DS."component.php";
+require_once _PHPFRAME_PATH.DS."application".DS."components.php";
 require_once _PHPFRAME_PATH.DS."application".DS."controller.php";
 require_once _PHPFRAME_PATH.DS."application".DS."debug.php";
 require_once _PHPFRAME_PATH.DS."application".DS."error.php";
 require_once _PHPFRAME_PATH.DS."application".DS."factory.php";
-require_once _PHPFRAME_PATH.DS."application".DS."menu.php";
 require_once _PHPFRAME_PATH.DS."application".DS."model.php";
+require_once _PHPFRAME_PATH.DS."application".DS."modules.php";
 require_once _PHPFRAME_PATH.DS."application".DS."pathway.php";
+require_once _PHPFRAME_PATH.DS."application".DS."permissions.php";
 require_once _PHPFRAME_PATH.DS."application".DS."route.php";
 require_once _PHPFRAME_PATH.DS."application".DS."view.php";
 require_once _PHPFRAME_PATH.DS."application".DS."user.php";
