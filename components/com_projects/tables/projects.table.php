87e51689e05b227888d4afe01ce9db69ce6413be
luis.montero <luis.montero@e-noise.com>
2009-02-16 18:50:23 +0000

    Porting com_projects table classes from IntranetOffice. Work in progress...

---
+++ components/com_projects/tables/projects.table.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * @version 	$Id$
+ * @package		ExtranetOffice
+ * @subpackage	com_projects
+ * @copyright	Copyright (C) 2009 E-noise.com Limited. All rights reserved.
+ * @license		BSD revised. See LICENSE.
+ * @author 		Luis Montero [e-noise.com]
+ */
+
+defined( '_EXEC' ) or die( 'Restricted access' );
+
+/**
+ * projectsTableProjects Class
+ * 
+ * @package		ExtranetOffice
+ * @subpackage 	com_projects
+ * @author 		Luis Montero [e-noise.com]
+ * @since 		1.0
+ */
+class projectsTableProjects extends table {
+	/**
+	 * The project id
+	 * 
+	 * int(11) auto_increment
+	 * 
+	 * @var int
+	 */
+	var $id=null;
+	/**
+	 * The project name
+	 * 
+	 * varchar(128) NOT NULL
+	 * 
+	 * @var string
+	 */
+	var $name=null;
+	/**
+	 * The project type id
+	 * 
+	 * tinyint
+	 * 
+	 * @var int
+	 */
+	var $project_type=null;
+	/**
+	 * Priority id
+	 * 
+	 * tinyint
+	 * 
+	 * @var int
+	 */
+	var $priority=null;
+	/**
+	 * The company id
+	 * 
+	 * int(11) NOT NULL
+	 * 
+	 * @var int
+	 */
+	var $company_id=null;
+	/**
+	 * The project description
+	 * 
+	 * text NOT NULL
+	 * 
+	 * @var string
+	 */
+	var $description=null;
+	/**
+	 * Project wide access level
+	 * 
+	 * tinyint(4)
+	 * 
+	 * @var int
+	 */
+	var $access=null;
+	/**
+	 * Status id
+	 * 
+	 * tinyint(1)	
+	 * 
+	 * @var int
+	 */
+	var $status=null;
+	/**
+	 * Issues access level
+	 * 
+	 * enum('1', '2', '3', '4') 1=Admins, 2=Project workers, 3=Guests, 4=Public
+	 * 
+	 * @var int
+	 */
+	var $access_issues=null;
+	/**
+	 * Milestones access level
+	 * 
+	 * enum('1', '2', '3', '4') 1=Admins, 2=Project workers, 3=Guests, 4=Public
+	 * 
+	 * @var int
+	 */
+	var $access_milestones=null;
+	/**
+	 * Files access level
+	 * 
+	 * enum('1', '2', '3', '4') 1=Admins, 2=Project workers, 3=Guests, 4=Public
+	 * 
+	 * @var int
+	 */
+	var $access_files=null;
+	/**
+	 * Meetings access level
+	 * 
+	 * enum('1', '2', '3', '4') 1=Admins, 2=Project workers, 3=Guests, 4=Public
+	 * 
+	 * @var int
+	 */
+	var $access_meetings=null;
+	/**
+	 * Reports access level
+	 * 
+	 * enum('1', '2', '3', '4') 1=Admins, 2=Project workers, 3=Guests, 4=Public
+	 * 
+	 * @var int
+	 */
+	var $access_reports=null;
+	/**
+	 * Polls access level
+	 * 
+	 * enum('1', '2', '3', '4') 1=Admins, 2=Project workers, 3=Guests, 4=Public
+	 * 
+	 * @var int
+	 */
+	var $access_polls=null;
+	/**
+	 * Messages access level
+	 * 
+	 * enum('1', '2', '3', '4') 1=Admins, 2=Project workers, 3=Guests, 4=Public
+	 * 
+	 * @var int
+	 */
+	var $access_messages=null;
+	/**
+	 * People access level
+	 * 
+	 * enum('1', '2', '3', '4') 1=Admins, 2=Project workers, 3=Guests, 4=Public
+	 * 
+	 * @var int
+	 */
+	var $access_people=null;
+	/**
+	 * Admin access level
+	 * 
+	 * enum('1', '2', '3', '4') 1=Admins, 2=Project workers, 3=Guests, 4=Public
+	 * 
+	 * @var int
+	 */
+	var $access_admin=null;
+	/**
+	 * User id of the project creator
+	 * 
+	 * int(11)
+	 * 
+	 * @var int
+	 */
+	var $created_by=null;
+	/**
+	 * Date created
+	 * 
+	 * datetime
+	 * 
+	 * @var string
+	 */
+	var $created=null;
+  
+	function __construct() {
+		$db =& factory::getDB();
+		parent::__construct( '#__projects', 'id', $db );
+	}
+}
+?>
---
+++ components/com_projects/tables/users_roles.table.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @version 	$Id$
+ * @package		ExtranetOffice
+ * @subpackage	com_projects
+ * @copyright	Copyright (C) 2009 E-noise.com Limited. All rights reserved.
+ * @license		BSD revised. See LICENSE.
+ * @author 		Luis Montero [e-noise.com]
+ */
+
+defined( '_EXEC' ) or die( 'Restricted access' );
+
+/**
+ * projectsTableUsersRoles Class
+ * 
+ * @package		ExtranetOffice
+ * @subpackage 	com_projects
+ * @author 		Luis Montero [e-noise.com]
+ * @since 		1.0
+ */
+class projectsTableUsersRoles extends JTable {
+	/**
+	 * Id
+	 * 
+	 * @var int
+	 */
+	var $id=null;
+	/**
+	 * User ID
+	 * 
+	 * @var int
+	 */
+	var $userid=null;
+	/**
+	 * Project ID
+	 * 
+	 * @var int
+	 */
+	var $projectid=null;
+	/**
+	 * Role ID
+	 * 
+	 * @var int
+	 */
+	var $roleid=null;
+  
+	function __construct() {
+		$db =& factory::getDB();
+		parent::__construct( '#__users_roles', 'id', $db );
+	}
+	
+	function load($userid, $projectid) {
+		$db =& factory::getDB();
+		$query = "SELECT * FROM #__users_roles WHERE userid = ".$userid." AND projectid = ".$projectid;
+		$db->setQuery($query);
+		$row = $db->loadAssoc();
+		if (is_array($row) && count($row) > 0) {
+			foreach ($row as $key=>$value) {
+				$this->$key = $value;
+			}
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+?>
