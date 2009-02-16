a82209c1a3a9c93f4b354c21f6b0ddf0ba522d68
luis.montero <luis.montero@e-noise.com>
2009-02-16 19:17:28 +0000

    Issues table ported from IntranetOffice in com_projects.

---
+++ components/com_projects/tables/issues.table.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * @version 	$Id$
+ * @package		ExtranetOffice
+ * @subpackage	com_projects
+ * @copyright	Copyright (C) 2009 E-noise.com Limited. All rights reserved.
+ * @license		BSD revised. See LICENSE.
+ * @author 		Luis Montero [e-noise.com]
+ */
+
+defined( '_EXEC' ) or die( 'Restricted access' );
+
+/**
+ * projectsTableIssues Class
+ * 
+ * @package		ExtranetOffice
+ * @subpackage 	com_projects
+ * @author 		Luis Montero [e-noise.com]
+ * @since 		1.0
+ */
+class projectsTableIssues extends table {
+	/**
+	 * The issue id
+	 * 
+	 * int(11) auto_increment
+	 * 
+	 * @var int
+	 */
+	var $id=null;
+	/**
+	 * The project id
+	 * 
+	 * int(11)
+	 * 
+	 * @var unknown_type
+	 */
+	var $projectid=null;
+	/**
+	 * Issue type id
+	 * 
+	 * int(11)
+	 * 
+	 * @var int
+	 */
+	var $issue_type=null;
+	/**
+	 * The issue title
+	 * 
+	 * varchar(128) NOT NULL
+	 * 
+	 * @var string
+	 */
+	var $title=null;
+	/**
+	 * The issue description
+	 * 
+	 * text NOT NULL
+	 * 
+	 * @var string
+	 */
+	var $description=null;
+	/**
+	 * The priority id
+	 * 
+	 * tinyint
+	 * 
+	 * @var int
+	 */
+	var $priority=null;
+	/**
+	 * Start date
+	 * 
+	 * date
+	 * 
+	 * @var string
+	 */
+	var $dtstart=null;
+	/**
+	 * End date
+	 * 
+	 * date
+	 * 
+	 * @var string
+	 */
+	var $dtend=null;
+	/**
+	 * Expected duration
+	 * 
+	 * float(4,2)
+	 * 
+	 * @var float
+	 */
+	var $expected_duration=null;
+	/**
+	 * Progress percentage
+	 * 
+	 * tinyint(3)
+	 * 
+	 * @var int
+	 */
+	var $progress=null;
+	/**
+	 * Access level
+	 * 
+	 * enum('0','1') 0=Public, 1=Private, only owner and assigned users
+	 * 
+	 * @var int
+	 */
+	var $access=null;
+	/**
+	 * User ID of issue creator
+	 * 
+	 * int(11)
+	 * 
+	 * @var int
+	 */
+	var $created_by=null;
+	/**
+	 * Date created
+	 * 
+	 * datetime
+	 * 
+	 * @var string
+	 */
+	var $created=null;
+	/**
+	 * User ID of user who closed the issue
+	 * 
+	 * int(11)
+	 * 
+	 * @var int
+	 */
+	var $closed_by=null;
+	/**
+	 * Date closed
+	 * 
+	 * datetime
+	 * 
+	 * @var string
+	 */
+	var $closed=null;
+	/**
+	 * MySQL timestamp
+	 * 
+	 * timestamp
+	 * 
+	 * @var string
+	 */
+	var $ts=null;
+  
+	function __construct() {
+		$db =& factory::getDB();
+		parent::__construct( '#__issues', 'id', $db );
+	}
+}
+?>
