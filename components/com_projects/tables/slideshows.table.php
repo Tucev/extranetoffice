9a184790f3203881d673ab76b6f47227fdce2303
luis.montero <luis.montero@e-noise.com>
2009-02-17 00:02:32 +0000

    Adding com_projects remaining table classes ported from IntranetOffice.

---
+++ components/com_projects/tables/slideshows.table.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version 	$Id$
+ * @package		ExtranetOffice
+ * @subpackage	com_projects
+ * @copyright	Copyright (C) 2009 E-noise.com Limited. All rights reserved.
+ * @license		BSD revised. See LICENSE.
+ * @author 		Luis Montero [e-noise.com]
+ */
+
+defined( '_EXEC' ) or die( 'Restricted access' );
+
+/**
+ * projectsTableSlideshows Class
+ * 
+ * @package		ExtranetOffice
+ * @subpackage 	com_projects
+ * @author 		Luis Montero [e-noise.com]
+ * @since 		1.0
+ */
+class projectsTableSlideshows extends table {
+	var $id=null; // int(11) auto_increment
+	var $projectid=null; // int(11)
+	var $meetingid=null; // int(11)
+	var $name=null; // varchar(64)
+	var $description=null; // text
+	var $created_by=null; // int(11)
+	var $created=null; // datetime
+  
+	function __construct() {
+		$db =& factory::getDB();
+		parent::__construct( '#__slideshows', 'id', $db );
+	}
+}
+?>
--- components/com_projects/tables/users_roles.table.php
+++ components/com_projects/tables/users_roles.table.php
@@ -18,7 +18,7 @@ defined( '_EXEC' ) or die( 'Restricted access' );
  * @author 		Luis Montero [e-noise.com]
  * @since 		1.0
  */
-class projectsTableUsersRoles extends JTable {
+class projectsTableUsersRoles extends table {
 	/**
 	 * Id
 	 * 
