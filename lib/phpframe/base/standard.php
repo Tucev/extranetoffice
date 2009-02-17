78cca2d0ea308104088523711b12c16dcb9ecc0a
luis.montero <luis.montero@e-noise.com>
2009-02-17 02:19:08 +0000

    Base objects directory renamed in phpFrame.

---
+++ lib/phpframe/base/standard.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		phpFrame
+ * @subpackage 	objects
+ * @copyright	Copyright (C) 2009 E-noise.com Limited. All rights reserved.
+ * @license		BSD revised. See LICENSE.
+ * @author 		Luis Montero [e-noise.com]
+ */
+
+defined( '_EXEC' ) or die( 'Restricted access' );
+
+/**
+ * Standard Object Class
+ * 
+ * This class provides a standard object class with some useful generic methods. 
+ * 
+ * @package		phpFrame
+ * @subpackage 	objects
+ * @author 		Luis Montero [e-noise.com]
+ * @since 		1.0
+ */
+class standardObject {
+	function get() {
+		
+	}
+	
+	function set() {
+		
+	}
+	
+	function toString() {
+		
+	}
+}
+?>
