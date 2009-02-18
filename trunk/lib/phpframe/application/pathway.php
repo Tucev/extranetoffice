cb2933084e0270a4e51396ef5215633afa4ac19a
luis.montero@e-noise.com <luis.montero@e-noise.com@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-02-18 02:52:24 +0000

    Pathway class added to phpFrame.

--- trunk/lib/phpframe/application/pathway.php
+++ trunk/lib/phpframe/application/pathway.php
@@ -19,6 +19,90 @@ defined( '_EXEC' ) or die( 'Restricted access' );
  * @since 		1.0
  */
 class pathway extends singleton {
+	/**
+	 * Array containing the pathway item objects
+	 * 
+	 * @var array
+	 */
+	var $pathway=null;
+	/**
+	 * Items counter
+	 * 
+	 * @var int
+	 */
+	var $count;
 	
+	/**
+	 * Constructor
+	 * 
+	 * @return 	void
+	 * @since	1.0
+	 */
+	function __construct() {
+		$this->pathway = array();
+		
+		$item = new standardObject();
+		$item->set('title', 'Home');
+		$item->set('url', 'index.php');
+		
+		$this->pathway[] = $item;
+	}
+	
+	/**
+	 * Add a pathway item
+	 * 
+	 * @param	string	$title The pathway item title.
+	 * @param 	string	$url The pathway item URL.
+	 * @return 	void
+	 * @since	1.0
+	 */
+	function addItem($title, $url) {
+		$item = new standardObject();
+		$item->set('title', $title);
+		$item->set('url', $url);
+		
+		$this->pathway[] = $item;
+	}
+	
+	/**
+	 * Set the pathway array
+	 * 
+	 * @param	array 	$pathway An array of pathway item objects.
+	 * @return 	array
+	 * @since	1.0
+	 */
+	function setPathway($pathway) {
+		$oldPathway = $this->pathway;
+		$pathway = (array) $pathway;
+        
+        // Set the new pathway.
+        $this->_pathway = array_values($pathway);
+        
+        return array_values($oldPathway);
+	}
+	
+	/**
+	 * Echo pathway as HTML
+	 * 
+	 * @return 	void
+	 * @since	1.0
+	 */
+	function display() {
+		echo '<div class="pathway">';
+		for ($i=0; $i<count($this->pathway); $i++) {
+			if ($i>0) {
+				echo ' &gt;&gt; ';
+			}
+			echo '<span class="pathway_item">';
+			if (!empty($this->pathway[$i]->url)) {
+				echo '<a href="'.$this->pathway[$i]->url.'">'.$this->pathway[$i]->title.'</a>';
+			}
+			else {
+				echo $this->pathway[$i]->title;
+			}
+			echo '</span>';
+		}
+		echo '</div>';
+	}
 }
 ?>
