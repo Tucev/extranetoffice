5f3274f8349ded406a18fdfcb5db7768bbd7879d
IQAbsent <iqabsent@gmail.com>
2009-02-20 15:53:14 +0000

    add stylesheet to printHead method minor changes to html document

--- lib/phpframe/document/html.php
+++ lib/phpframe/document/html.php
@@ -76,6 +76,14 @@ class documentHTML extends document {
 	var $_type = null;
 	
 	/**
+	 * Array for different tag types to be printed in <head></head>
+	 *
+	 * @var	 string
+	 * @access  private
+	 */
+	var $_tagTypes = array("_scripts_linked","_styles_linked");
+	
+	/**
 	 * Add linked scrip in document head
 	 * 
 	 * It takes both relative and absolute values
@@ -85,10 +93,7 @@ class documentHTML extends document {
 	 * @return void
 	 */
 	function addScript($src, $type='text/javascript') {
-		// Add the document base if a relative path
-		if (substr($src, 0, 4) != 'http') {
-			$src = $this->base.$src;
-		}
+		$this->_makeAbsolute($src);
 		$this->_scripts_linked[] = '<script type="'.$type.'" src="'.$src.'"></script>';
 	}
 	
@@ -100,6 +105,7 @@ class documentHTML extends document {
 	 * @return void
 	 */
 	function addStyleSheet($href, $type='text/css') {
+		$this->_makeAbsolute($href);
 		$this->_styles_linked[] = '<link rel="stylesheet" href="'.$href.'" type="'.$type.'" />';
 	}
 	
@@ -109,11 +115,26 @@ class documentHTML extends document {
 	 * @return void
 	 */
 	function printHead() {
-		if (is_array($this->_scripts_linked) && count($this->_scripts_linked) > 0) {
-			echo implode($this->_lineEnd, $this->_scripts_linked).$this->_lineEnd;
-		}
 		
-		//echo '<pre>'; var_dump($this); echo '</pre>';
+		// For each tag type
+		foreach($this->_tagTypes as $tagType){
+			if (is_array($this->$tagType) && count($this->$tagType) > 0) {
+				echo implode($this->_lineEnd, $this->$tagType).$this->_lineEnd;
+			}
+		}
+	}
+	
+	/**
+	 * Make a path absolute
+	 * 
+	 * @return string
+	 */
+	private function _makeAbsolute(&$path)
+	{
+		// Add the document base if a relative path
+		if (substr($path, 0, 4) != 'http') {
+			$path = $this->base.$path;
+		}
 	}
 }
 ?>
