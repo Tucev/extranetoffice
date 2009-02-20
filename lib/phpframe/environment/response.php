278d52f41257459c5aace7569f3b8253f5151e62
luis.montero <luis.montero@e-noise.com>
2009-02-20 02:00:31 +0000

    This class still needs to be written, but I'm commiting the file because otherwise you will get an error when running the app this file is being included by the framework.

---
+++ lib/phpframe/environment/response.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		phpFrame
+ * @subpackage 	environment
+ * @copyright	Copyright (C) 2009 E-noise.com Limited. All rights reserved.
+ * @license		BSD revised. See LICENSE.
+ * @author 		Luis Montero [e-noise.com]
+ */
+
+defined( '_EXEC' ) or die( 'Restricted access' );
+
+/**
+ * Response Class
+ * 
+ * @package		phpFrame
+ * @subpackage 	environment
+ * @author 		Luis Montero [e-noise.com]
+ * @since 		1.0
+ */
+class response {
+	
+	function setHeader() {
+		
+	}
+	
+}
+?>
