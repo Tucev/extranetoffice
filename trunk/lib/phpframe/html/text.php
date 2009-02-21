f1a3a3e641fd0e7761c3cc9a3e2c2d62f97463dc
luis.montero@e-noise.com <luis.montero@e-noise.com@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-02-21 18:14:15 +0000

    Added bytes() and limit_words() formatting methods in phpFrame's "text" class.

--- trunk/lib/phpframe/html/text.php
+++ trunk/lib/phpframe/html/text.php
@@ -14,5 +14,25 @@ class text {
 	function _($str) {
 		return $str;
 	}
+	
+	function bytes($a) {
+	    $unim = array("B","KB","MB","GB","TB","PB");
+	    $c = 0;
+	    while ($a>=1024) {
+	        $c++;
+	        $a = $a/1024;
+	    }
+	    return number_format($a,($c ? 2 : 0),",",".")." ".$unim[$c];
+	}
+	
+	function limit_words($str, $max_chars, $add_trailing_dots=true) {
+		if (strlen($str) > $max_chars) {
+			$str = substr($str, 0, $max_chars);
+			$str = substr($str, 0, strrpos($str, " "));
+			if ($add_trailing_dots === true) $str .= " ...";
+		}
+		
+		return $str;
+	}
 }
 ?>
