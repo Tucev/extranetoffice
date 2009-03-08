457685db203b2bb15240f552efcc1c6deacb62f2
Luis Montero <Luis Montero@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-03-08 17:56:57 +0000

    phpFrame filesystem and image classes added un utils subpackage. Also added phpFrame's own language file (used for error messages).

---
+++ trunk/lib/phpframe/lang/en-GB.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		phpFrame
+ * @subpackage 	lang
+ * @copyright	Copyright (C) 2009 E-noise.com Limited. All rights reserved.
+ * @license		BSD revised. See LICENSE.
+ * @author 		Luis Montero [e-noise.com]
+ */
+
+defined( '_EXEC' ) or die( 'Restricted access' );
+
+// utils > filesystem
+define("_PHPFRAME_LANG_UPLOAD_ERROR_PHP_UP_MAX_FILESIZE", "ERROR: PHP upload maximum file size exceeded!");
+define("_PHPFRAME_LANG_UPLOAD_ERROR_PHP_MAX_FILESIZE", "ERROR: PHP maximum file size exceeded!");
+define("_PHPFRAME_LANG_UPLOAD_ERROR_PARTIAL_UPLOAD", "ERROR: Partial upload!");
+define("_PHPFRAME_LANG_UPLOAD_ERROR_NO_FILE", "ERROR: No file submitted for upload!");
+define("_PHPFRAME_LANG_UPLOAD_ERROR_MAX_FILESIZE", "ERROR: Maximum file size exceeded!");
+define("_PHPFRAME_LANG_UPLOAD_ERROR_FILETYPE", "ERROR: File type not valid!");
+define("_PHPFRAME_LANG_UPLOAD_ERROR_MOVE", "ERROR: Could not move file to destination directory!");
+define("_PHPFRAME_LANG_UPLOAD_ERROR_ATTACK", "ERROR: Possible file attack!");
+?>
