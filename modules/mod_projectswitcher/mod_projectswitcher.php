2631329a9a9a49d25223d75df5d6b63d1fb77b66
Luis Montero <Luis Montero@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-03-09 12:53:32 +0000

    Minor fix to mod_projectswitcher. Changed the subpackage in javadoc comments.

--- modules/mod_projectswitcher/mod_projectswitcher.php
+++ modules/mod_projectswitcher/mod_projectswitcher.php
@@ -2,7 +2,7 @@
 /**
  * @version 	$Id$
  * @package		ExtranetOffice
- * @subpackage 	mod_projects
+ * @subpackage 	mod_projectswitcher
  * @copyright	Copyright (C) 2009 E-noise.com Limited. All rights reserved.
  * @license		BSD revised. See LICENSE.
  * @author 		Luis Montero [e-noise.com]
