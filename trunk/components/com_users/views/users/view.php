b06ca5bb420d920061acd68ceea22998209857be
Luis Montero <Luis Montero@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-03-17 20:36:06 +0000

    Basic "users" view in refactored com_users component.

---
+++ trunk/components/com_users/views/users/view.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @version 	$Id$
+ * @package		ExtranetOffice
+ * @subpackage	com_users
+ * @copyright	Copyright (C) 2009 E-noise.com Limited. All rights reserved.
+ * @license		BSD revised. See LICENSE.
+ * @author 		Luis Montero [e-noise.com]
+ */
+
+defined( '_EXEC' ) or die( 'Restricted access' );
+
+/**
+ * usersViewUsers Class
+ * 
+ * The methods in this class are invoked by its parent class. See display() 
+ * method in 'view' class.
+ * 
+ * Method name to be triggered will be formed as follows:
+ * 
+ * <code>
+ * $tmpl_specific_method = "display".ucfirst(request::getVar('view')).ucfirst($this->tmpl);
+ * </code>
+ * 
+ * @package		ExtranetOffice
+ * @subpackage 	com_users
+ * @author 		Luis Montero [e-noise.com]
+ * @since 		1.0
+ * @see 		view, controller
+ */
+class usersViewUsers extends view {
+	var $page_title=null;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @return 	void
+	 * @since	1.0
+	 */
+	function __construct() {
+		// Set the view template to load (default value is set in controller)
+		$this->layout =& request::getVar('layout');
+		
+		parent::__construct();
+	}
+	
+	/**
+	 * Override view display method
+	 * 
+	 * This method overrides the parent display() method and appends the page title to the document title.
+	 * 
+	 * @return	void
+	 * @since	1.0
+	 */
+	function display() {
+		parent::display();
+		
+		// Append page title to document title
+		if (request::getVar('layout') != 'list') {
+			$document =& factory::getDocument('html');
+			$document->title .= ' - '.$this->page_title;
+		}
+	}
+	
+	/**
+	 * Custom display method triggered by list layout.
+	 * 
+	 * @return void
+	 */
+	function displayUsersList() {
+		$this->page_title = _LANG_USERS;
+		
+		$modelUsers =& $this->getModel('users');
+		$users = $modelUsers->getUsers();
+		$this->rows =& $users['rows'];
+		$this->pageNav =& $users['pageNav'];
+		$this->lists =& $users['lists'];
+	}
+	
+	/**
+	 * Custom display method triggered by detail layout.
+	 * 
+	 * @return void
+	 */
+	function displayUsersDetail() {
+		$userid = request::getVar('userid', 0);
+		
+		$modelUsers =& $this->getModel('users');
+		$this->row = $modelUsers->getUsersDetail($userid);
+		
+		$this->page_title = $this->row->firstname.' '.$this->row->lastname;
+		$this->addPathwayItem($this->row->firstname.' '.$this->row->lastname);
+	}
+}
+?>
