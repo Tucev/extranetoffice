5ec64eb5706f4bee920b6848affb7fe7726f312e
Luis Montero <Luis Montero@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-03-23 12:42:51 +0000

    Error message moved from template to modules.

---
+++ modules/mod_errormsg/mod_errormsg.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		ExtranetOffice
+ * @subpackage 	mod_errormsg
+ * @copyright	Copyright (C) 2009 E-noise.com Limited. All rights reserved.
+ * @license		BSD revised. See LICENSE.
+ * @author 		Luis Montero [e-noise.com]
+ */
+
+defined( '_EXEC' ) or die( 'Restricted access' );
+?>
+
+<div id="error_msg">
+	<?php error::display(); ?>
+</div>
