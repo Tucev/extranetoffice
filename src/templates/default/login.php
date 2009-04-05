74e2dcde370605a933136ff90d7ddaf3b219a486
Luis Montero <Luis Montero@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-04-05 23:36:48 +0000

    Show revision number in login template.

--- src/templates/default/login.php
+++ src/templates/default/login.php
@@ -41,7 +41,7 @@ defined( '_EXEC' ) or die( 'Restricted access' );
 <?php echo $this->component_output; ?>
 
 <div id="footer">
-Extranet Office 1.0 Alpha $Id$<br />
+Extranet Office 1.0 Alpha $Revision$<br />
 &copy; 2009 E-noise.com Limited
 </div>
 
