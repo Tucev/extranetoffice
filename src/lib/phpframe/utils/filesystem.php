763f5549d9356fc991b31628f7cfa1b9aadcdcc5
Luis Montero <Luis Montero@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-04-05 04:00:13 +0000

    phpFrame filesystem class renamed to avoid conflict with Phing.

--- src/lib/phpframe/utils/filesystem.php
+++ src/lib/phpframe/utils/filesystem.php
@@ -18,7 +18,7 @@ defined( '_EXEC' ) or die( 'Restricted access' );
  * @author 		Luis Montero [e-noise.com]
  * @since 		1.0
  */
-class filesystem {
+class phpFrameFilesystem {
 	/**
 	 * Upload file
 	 * 
