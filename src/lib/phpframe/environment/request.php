7b999b6d47c4a0bf7dc18f3dee8fcaae56600999
Luis Montero <Luis Montero@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-04-08 22:23:38 +0000

    phpFrame_Environment_Request class updated to parse command line arguments using regular expresion instead of explode(). Also made init() unset the superglobal request arrays to prevent them from being used and added some comments...

--- src/lib/phpframe/environment/request.php
+++ src/lib/phpframe/environment/request.php
@@ -12,6 +12,11 @@ defined( '_EXEC' ) or die( 'Restricted access' );
 /**
  * Request Class
  * 
+ * This class encapsulates access to the request arrays and provides input filtering.
+ * 
+ * All properties and methods in this class are static. Instantiation is prevented by 
+ * the constructor being declared as private.
+ * 
  * @todo		This class needs to be rewritten to use php's filter extension
  * @package		phpFrame
  * @subpackage 	environment
@@ -19,52 +24,106 @@ defined( '_EXEC' ) or die( 'Restricted access' );
  * @since 		1.0
  */
 class phpFrame_Environment_Request {
+	/**
+	 * The filtered $_REQUEST array
+	 * 
+	 * @static
+	 * @access	private
+	 * @var		array
+	 */
 	private static $_request=array();
+	/**
+	 * The filtered $_GET array
+	 * 
+	 * @static
+	 * @access	private
+	 * @var		array
+	 */
 	private static $_get=array();
+	/**
+	 * The filtered $_POST array
+	 * 
+	 * @static
+	 * @access	private
+	 * @var		array
+	 */
 	private static $_post=array();
+	/**
+	 * Instance of PHPInputFilter
+	 * 
+	 * @static
+	 * @access	private
+	 * @var		object
+	 */
+	private static $_inputfilter=null;
+	
+	/**
+	 * Private constructor prevents instantiation. This class should always be used statically.
+	 * 
+	 * @access	private
+	 * @return	void
+	 * @since	1.0
+	 */
+	private function __construct() {}
 	
 	/**
 	 * Constructor
 	 * 
 	 * Initialise the request, filter input and return the request array.
 	 * 
+	 * @static
+	 * @access	public
 	 * @return	array
 	 * @since	1.0
 	 */
 	public static function init() {
-		$inputfilter = new InputFilter();
+		// Before processing request data we check wheter the request has already been
+		// initialised by testing to see if we already have an instance of the input filter
+		if (self::$_inputfilter instanceof InputFilter) {
+			return;
+		}
+		
+		// Get an instance of PHP Input filter
+		self::$_inputfilter = new InputFilter();
 		
 		// Process incoming request arrays and store filtered data in class
-		self::$_request = $inputfilter->process($_REQUEST);
-		self::$_post = $inputfilter->process($_POST);
-		self::$_get = $inputfilter->process($_GET);
+		self::$_request = self::$_inputfilter->process($_REQUEST);
+		self::$_get = self::$_inputfilter->process($_GET);
+		self::$_post = self::$_inputfilter->process($_POST);
+		
+		// Once the superglobal request arrays are processed we unset them
+		// to prevent them being used from here on
+		unset($_REQUEST, $_GET, $_POST);
 		
 		// Get arguments passed via command line and parse them as request vars
 		global $argv;
-		
 		for ($i=1; $i<count($argv); $i++) {
-			$pair_array = explode('=', $argv[$i]);
-			self::$_request[$pair_array[0]] = $pair_array[1];
-			self::$_post[$pair_array[0]] = $pair_array[1];
+			if (preg_match('/^(.*)=(.*)$/', $argv[$i], $matches)) {
+				self::$_request[$matches[1]] = $matches[2];
+				self::$_post[$matches[1]] = $matches[2];
+			}
 		}
 	}
 	
 	/**
 	 * Get get or post global array
 	 * 
+	 * @static
+	 * @access	public
 	 * @param	string	$global_key "get", "post" or "request"
 	 * @return	array
+	 * @since	1.0
 	 */
 	public static function get($global_key) {
 		switch ($global_key) {
+			case 'request' : 
+				$array = self::$_request;
+				break;
 			case 'get' : 
 				$array = self::$_get;
 				break;
 			case 'post' : 
 				$array = self::$_post;
-				break;
-			case 'request' : 
-				$array = self::$_request;
 				break;
 		}
 		
@@ -79,17 +138,18 @@ class phpFrame_Environment_Request {
 	/**
 	 * Get request variable
 	 * 
+	 * @static
+	 * @access	public
 	 * @param	string	$name The array key.
 	 * @param	mixed	$default The default value used to initialise the variable if null.
 	 * @return	mixed
 	 * @since	1.0
 	 */
 	public static function getVar($key, $default='') {
-		$inputfilter = new InputFilter();
-		
 		// Set default value if var is empty
 		if (!isset(self::$_request[$key]) || self::$_request[$key] == '') {
-			self::$_request[$key] = $inputfilter->process($default);
+			// Filter value before assigning to request arrays
+			self::$_request[$key] = self::$_inputfilter->process($default);
 		}
 		
 		return self::$_request[$key];
@@ -98,30 +158,35 @@ class phpFrame_Environment_Request {
 	/**
 	 * Set request variable.
 	 * 
+	 * @static
+	 * @access	public
 	 * @param	string	$name The array key.
 	 * @param	mixed	$value The value to set the variable to.
 	 * @return	void
 	 * @since	1.0
 	 */
 	public static function setVar($key, $value) {
-		$inputfilter = new InputFilter();
-		$value = $inputfilter->process($value);
+		// Filter value before assigning to request arrays
+		$value = self::$_inputfilter->process($value);
 		
 		self::$_request[$key] = $value;
 		self::$_post[$key] = $value;
-		$_REQUEST[$key] = $value;
-		$_POST[$key] = $value;
 		
 		return $value;
 	}
 	
+	/**
+	 * Destroy the request data
+	 * 
+	 * @static
+	 * @access	public
+	 * @return	void
+	 * @since	1.0
+	 */
 	public static function destroy() {
 		self::$_request = array();
 		self::$_post = array();
 		self::$_get = array();
-		$_REQUEST = array();
-		$_POST = array();
-		$_GET = array();
 	}
 }
 ?>
