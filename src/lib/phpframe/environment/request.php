fc662a2d0fc4df438ec5f77472fad52562eca8b7
iqabsent <iqabsent@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-04-22 17:30:04 +0000

    Changes made to request class (lib.phpframe.environment.request.php) Unified Request Array now in use component, action, view & layout now seperate private variables with getters & setters

--- src/lib/phpframe/environment/request.php
+++ src/lib/phpframe/environment/request.php
@@ -25,29 +25,54 @@ defined( '_EXEC' ) or die( 'Restricted access' );
  */
 class phpFrame_Environment_Request {
 	/**
-	 * The filtered $_REQUEST array
+	 * A unification array of filtered global arrays
 	 * 
 	 * @static
 	 * @access	private
 	 * @var		array
 	 */
-	private static $_request=array();
+	private static $_URA=array();
 	/**
-	 * The filtered $_GET array
+	 * Array list of core variable names
 	 * 
 	 * @static
 	 * @access	private
 	 * @var		array
 	 */
-	private static $_get=array();
+	private static $_core_variables=array('component','action','view','layout');
 	/**
-	 * The filtered $_POST array
+	 * A core request variable specifying controller/component
 	 * 
 	 * @static
 	 * @access	private
-	 * @var		array
+	 * @var		string
 	 */
-	private static $_post=array();
+	private static $_component;
+	/**
+	 * A core request variable specifying controller/component functino to execute
+	 * 
+	 * @static
+	 * @access	private
+	 * @var		string
+	 */
+	private static $_action;
+	/**
+	 * A core request variable specifying which view to render
+	 * 
+	 * @static
+	 * @access	private
+	 * @var		string
+	 */
+	private static $_view;	
+	/**
+	 * A core request variable specifying which view layout to use
+	 * 
+	 * @static
+	 * @access	private
+	 * @var		string
+	 */
+	private static $_layout;
+	
 	/**
 	 * Instance of PHPInputFilter
 	 * 
@@ -76,63 +101,66 @@ class phpFrame_Environment_Request {
 	 * @return	array
 	 * @since	1.0
 	 */
-	public static function init() {
+	public static function init($client='http') {
 		// Before processing request data we check wheter the request has already been
-		// initialised by testing to see if we already have an instance of the input filter
-		if (self::$_inputfilter instanceof InputFilter) {
+		// initialised by testing to see if $_URA is populated
+		if (!empty(self::$_URA)) {
 			return;
 		}
 		
-		// Get an instance of PHP Input filter
-		self::$_inputfilter = new InputFilter();
+		//TODO create instance of appropriate request helper to populate $_URA
+		//TODO stop ignoring $_SESSION and $_COOKIES
 		
-		// Process incoming request arrays and store filtered data in class
-		self::$_request = self::$_inputfilter->process($_REQUEST);
-		self::$_get = self::$_inputfilter->process($_GET);
-		self::$_post = self::$_inputfilter->process($_POST);
+		if ($client == 'http') {
+			// Get an instance of PHP Input filter
+			self::$_inputfilter = new InputFilter();
+			
+			// Process incoming request arrays and store filtered data in class
+			self::$_URA['request'] = self::$_inputfilter->process($_REQUEST);
+			self::$_URA['get'] = self::$_inputfilter->process($_GET);
+			self::$_URA['post'] = self::$_inputfilter->process($_POST);
+			
+			// Once the superglobal request arrays are processed we unset them
+			// to prevent them being used from here on
+			unset($_REQUEST, $_GET, $_POST);
+		}
 		
-		// Once the superglobal request arrays are processed we unset them
-		// to prevent them being used from here on
-		unset($_REQUEST, $_GET, $_POST);
-		
-		// Get arguments passed via command line and parse them as request vars
-		global $argv;
-		for ($i=1; $i<count($argv); $i++) {
-			if (preg_match('/^(.*)=(.*)$/', $argv[$i], $matches)) {
-				self::$_request[$matches[1]] = $matches[2];
-				self::$_post[$matches[1]] = $matches[2];
+		if ($client == 'cli') {
+			// Get arguments passed via command line and parse them as request vars
+			global $argv;
+			$request = array();
+			for ($i=1; $i<count($argv); $i++) {
+				if (preg_match('/^(.*)=(.*)$/', $argv[$i], $matches)) {
+					$request[$matches[1]] = $matches[2];
+				}
 			}
-		}
-	}
-	
-	/**
-	 * Get get or post global array
-	 * 
-	 * @static
-	 * @access	public
-	 * @param	string	$global_key "get", "post" or "request"
-	 * @return	array
-	 * @since	1.0
-	 */
-	public static function get($global_key) {
-		switch ($global_key) {
-			case 'request' : 
-				$array = self::$_request;
-				break;
-			case 'get' : 
-				$array = self::$_get;
-				break;
-			case 'post' : 
-				$array = self::$_post;
-				break;
+			self::$_URA['request'] = $request;
 		}
 		
-		if ($array) {
-			return $array;
+		//add other globals
+		self::$_URA['env'] = $_ENV;
+		self::$_URA['server'] = $_SERVER;
+		self::$_URA['files'] = $_FILES;
+		
+		//TODO possible optimisation, see following:
+		/*
+		foreach (self::$_core_variables as $key) {
+			$core_variable_name = '_'.$key;
+			//self::$$core_variable_name = self::$_URA['request'][$key];
+			self::$$core_variable_name = self::$_URA['request'][$key];
+			unset(self::$_URA['request'][$key]);
 		}
-		else {
-			return false;
-		}
+		*/
+		
+		//rip core variables from URA
+		self::$_component = self::$_URA['request']['component'];
+		unset(self::$_URA['request']['component']);
+		self::$_action = self::$_URA['request']['action'];
+		unset(self::$_URA['request']['action']);
+		self::$_view = self::$_URA['request']['view'];
+		unset(self::$_URA['request']['view']);
+		self::$_layout = self::$_URA['request']['layout'];
+		unset(self::$_URA['request']['layout']);
 	}
 	
 	/**
@@ -147,12 +175,12 @@ class phpFrame_Environment_Request {
 	 */
 	public static function getVar($key, $default='') {
 		// Set default value if var is empty
-		if (!isset(self::$_request[$key]) || self::$_request[$key] == '') {
+		if (!isset(self::$_URA[$key]) || self::$_URA[$key] == '') {
 			// Filter value before assigning to request arrays
-			self::$_request[$key] = self::$_inputfilter->process($default);
+			self::$_URA[$key] = self::$_inputfilter->process($default);
 		}
 		
-		return self::$_request[$key];
+		return self::$_URA[$key];
 	}
 	
 	/**
@@ -166,11 +194,116 @@ class phpFrame_Environment_Request {
 	 * @since	1.0
 	 */
 	public static function setVar($key, $value) {
-		// Filter value before assigning to request arrays
+		// Filter value before assigning to request arrays 
+		
+		return self::$_URA['request'][$key] = self::$_inputfilter->process($value);
+	}
+	
+	/**
+	 * Get $_component.
+	 * 
+	 * @static
+	 * @access	public
+	 * @return	component
+	 */
+	public static function getComponent() {
+		
+		return self::$_component;
+	}
+	/**
+	 * Set $_component.
+	 * 
+	 * @static
+	 * @access	public
+	 * @param	string	$value The value to set the variable to.
+	 * @return	void
+	 */
+	public static function setComponent($value) {
+		// Filter value before assigning to variable
 		$value = self::$_inputfilter->process($value);
 		
-		self::$_request[$key] = $value;
-		self::$_post[$key] = $value;
+		self::$_component = $value;
+		
+		return $value;
+	}
+	/**
+	 * Get $_action
+	 * 
+	 * @static
+	 * @access	public
+	 * @return	action
+	 */
+	public static function getAction() {
+		
+		return self::$_action;
+	}
+	/**
+	 * Set $_action.
+	 * 
+	 * @static
+	 * @access	public
+	 * @param	string	$value The value to set the variable to.
+	 * @return	void
+	 */
+	public static function setAction($value) {
+		// Filter value before assigning to variable
+		$value = self::$_inputfilter->process($value);
+		
+		self::$_action = $value;
+		
+		return $value;
+	}
+	/**
+	 * Get $_view
+	 * 
+	 * @static
+	 * @access	public
+	 * @return	action
+	 */
+	public static function getView() {
+		
+		return self::$_view;
+	}
+	/**
+	 * Set $_view.
+	 * 
+	 * @static
+	 * @access	public
+	 * @param	string	$value The value to set the variable to.
+	 * @return	void
+	 */
+	public static function setView($value) {
+		// Filter value before assigning to variable
+		$value = self::$_inputfilter->process($value);
+		
+		self::$_view = $value;
+		
+		return $value;
+	}
+	/**	
+	 * Get $_layout
+	 * 
+	 * @static
+	 * @access	public
+	 * @return	layout
+	 */
+	public static function getLayout() {
+		
+		return self::$_layout;
+	}
+	/**
+	 * Set $_layout.
+	 * 
+	 * @static
+	 * @access	public
+	 * @param	string	$value The value to set the variable to.
+	 * @return	void
+	 */
+	public static function setLayout($value) {
+		// Filter value before assigning to variable
+		$value = self::$_inputfilter->process($value);
+		
+		self::$_layout = $value;
 		
 		return $value;
 	}
@@ -184,9 +317,7 @@ class phpFrame_Environment_Request {
 	 * @since	1.0
 	 */
 	public static function destroy() {
-		self::$_request = array();
-		self::$_post = array();
-		self::$_get = array();
+		self::$_URA = array();
 	}
 }
 ?>
