c50235819384a7bf005cb003d9219b70bd7b46ab
Luis Montero <Luis Montero@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-04-30 22:57:29 +0000

    Autoload class updated to use regular expression to figure out components classes file paths.

--- src/inc/autoload.php
+++ src/inc/autoload.php
@@ -17,75 +17,70 @@ spl_autoload_register('__autoload');
  * hasn't been defined yet. By calling this function the scripting engine is given a last 
  * chance to load the class before PHP fails with an error. 
  * 
- * @param	string	$className
+ * @param	string	$class_name
  * @return	void
  */
-function __autoload($className) {
+function __autoload($class_name) {
 	// phpFrame classes
-	if (strpos($className, 'phpFrame') !== false) {
-		$array = explode('_', $className);
+	if (strpos($class_name, 'phpFrame') !== false) {
+		$array = explode('_', $class_name);
 		if (sizeof($array) == 3) {
-			$file_path = _ABS_PATH.DS."lib".DS."phpframe".DS.strtolower($array[1]).DS.strtolower($array[2]).".php";
+			$file_path = "lib".DS."phpframe".DS.strtolower($array[1]).DS.strtolower($array[2]).".php";
 		}
 		elseif (sizeof($array) == 2) {
-			$file_path = _ABS_PATH.DS."lib".DS."phpframe".DS.strtolower($array[1]).DS.strtolower($array[1]).".php";
+			$file_path = "lib".DS."phpframe".DS.strtolower($array[1]).DS.strtolower($array[1]).".php";
 		}
 		elseif (sizeof($array) == 1) {
-			$file_path = _ABS_PATH.DS."lib".DS."phpframe".DS."phpframe.php";
-		}
-	}
-	// MVC classes
-	elseif (strpos($className, 'Model') !== false) {
-		$array = explode('Model', $className);
-		if (sizeof($array) == 2) {
-			$file_path = _ABS_PATH.DS."components".DS.phpFrame_Environment_Request::getComponent().DS;
-			$file_path .= "models".DS.strtolower($array[1]).".php";
-		}
-	}
-	elseif (strpos($className, 'View') !== false) {
-		$array = explode('View', $className);
-		if (sizeof($array) == 2) {
-			$file_path = _ABS_PATH.DS."components".DS.phpFrame_Environment_Request::getComponent().DS;
-			$file_path .= "views".DS.strtolower($array[1]).DS."view.php";
-		}
-	}
-	elseif (strpos($className, 'Controller') !== false) {
-		$file_path = _ABS_PATH.DS."components".DS.phpFrame_Environment_Request::getComponent().DS."controller.php";
-	}
-	elseif (strpos($className, 'Helper') !== false) {
-		$array = explode('Helper', $className);
-		if (sizeof($array) == 2) {
-			$file_path = _ABS_PATH.DS."components".DS.phpFrame_Environment_Request::getComponent().DS;
-			$file_path .= "helpers".DS.strtolower($array[1]).".helper.php";
-		}
-	}
-	elseif (strpos($className, 'Table') !== false) {
-		$array = explode('Table', $className);
-		if (sizeof($array) == 2) {
-			
-			if (preg_match('/^[A-Z]{1}[a-z]+([A-Z]{1})/', $array[1], $matches)) {
-				$array[1] = substr($array[1], 0, strpos($array[1], $matches[1]))."_".substr($array[1], strpos($array[1], $matches[1]));
-			}
-			
-			$file_path = _ABS_PATH.DS."components".DS.phpFrame_Environment_Request::getComponent().DS;
-			$file_path .= "tables".DS.strtolower($array[1]).".table.php";
+			$file_path = "lib".DS."phpframe".DS."phpframe.php";
 		}
 	}
 	// PHPMailer
-	elseif ($className == 'PHPMailer') {
-		$file_path = _ABS_PATH.DS."lib".DS."phpmailer".DS."phpmailer.php"; 
+	elseif ($class_name == 'PHPMailer') {
+		$file_path = "lib".DS."phpmailer".DS."phpmailer.php";
 	}
 	// PHPInputFilter
-	elseif ($className == 'InputFilter') {
-		$file_path = _ABS_PATH.DS."lib".DS."phpinputfilter".DS."inputfilter.php"; 
+	elseif ($class_name == 'InputFilter') {
+		$file_path = "lib".DS."phpinputfilter".DS."inputfilter.php";
+	}
+	elseif ($class_name == 'vCard') {
+		$file_path = "lib".DS."bitfolge".DS."vcard.php";
 	}
 	
-	// require th file if it exists, otherwise we throw an exception
-	if (file_exists($file_path)) {
+	else {
+		// Components classes
+		preg_match('/^([a-z]+)([A-Z]{1}[a-z]+)([A-Z]{1}[a-z]+)?([A-Z]{1}[a-z]+)?$/', $class_name, $matches);
+		if (is_array($matches) && count($matches) > 1) {
+			$file_path = "components".DS."com_".strtolower($matches[1]).DS;
+			
+			switch ($matches[2]) {
+				case 'Controller' : 
+					$file_path .= "controller.php";
+					break;
+				case 'Model' : 
+					$file_path .= "models".DS.strtolower($matches[3]).".php";
+					break;
+				case 'View' : 
+					$file_path .= "views".DS.strtolower($matches[3]).DS."view.php";
+					break;
+				case 'Helper' : 
+					$file_path .= "helpers".DS.strtolower($matches[3]).".helper.php";
+					break;
+				case 'Table' : 
+					$file_path .= "tables".DS.strtolower($matches[3]);
+					if (isset($matches[4])) $file_path .= "_".$matches[4];
+					$file_path .=".table.php";
+					break;
+			}
+		}
+		
+	}
+	
+	// require the file if it exists, otherwise we throw an exception
+	$file_path = _ABS_PATH.DS.$file_path;
+	if (is_file($file_path)) {
 		require $file_path;
 	}
 	else {
-		throw new phpFrame_Exception('Could not autoload class '.$className.'. File '.$file_path.' not found.');
+		throw new phpFrame_Exception('Could not autoload class '.$class_name.'. File '.$file_path.' not found.');
 	}
 }
-?>
