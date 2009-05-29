be885da8d7e73c9bafcd3e22bccac56310b040a4
Luis Montero <Luis Montero@bebdaaa6-f931-11dd-a8e2-f7cb3c35fcff>
2009-05-29 19:58:06 +0000

    Fixed problem with project saveRow() ensuring uploads directory is writable using absolute path.

--- src/components/com_projects/models/projects.php
+++ src/components/com_projects/models/projects.php
@@ -135,8 +135,8 @@ class projectsModelProjects extends phpFrame_Application_Model {
 		// Create filesystem directories if they don't exist yet
 		phpFrame_Utils_Filesystem::ensureWritableDir(config::FILESYSTEM.DS."projects");
 		phpFrame_Utils_Filesystem::ensureWritableDir(config::FILESYSTEM.DS."projects".DS.$row->id);
-		phpFrame_Utils_Filesystem::ensureWritableDir(config::UPLOAD_DIR.DS."projects");
-		phpFrame_Utils_Filesystem::ensureWritableDir(config::UPLOAD_DIR.DS."projects".DS.$row->id);
+		phpFrame_Utils_Filesystem::ensureWritableDir(_ABS_PATH.DS.config::UPLOAD_DIR.DS."projects");
+		phpFrame_Utils_Filesystem::ensureWritableDir(_ABS_PATH.DS.config::UPLOAD_DIR.DS."projects".DS.$row->id);
 		
 		return $row;
 	}
